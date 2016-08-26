c27cc5e2c03f35c052786087d04e79d1278ba011
Asheesh Laroia <asheesh@sandstorm.io>
2016-08-26 15:35:47 -0400

    Switch to lemp stack

--- .sandstorm/launcher.sh
+++ .sandstorm/launcher.sh
@@ -1,32 +1,33 @@
 #!/bin/bash
-set -euo pipefail
-# This script is run every time an instance of our app - aka grain - starts up.
-# This is the entry point for your application both when a grain is first launched
-# and when a grain resumes after being previously shut down.
-#
-# This script is responsible for launching everything your app needs to run.  The
-# thing it should do *last* is:
-#
-#   * Start a process in the foreground listening on port 8000 for HTTP requests.
-#
-# This is how you indicate to the platform that your application is up and
-# ready to receive requests.  Often, this will be something like nginx serving
-# static files and reverse proxying for some other dynamic backend service.
-#
-# Other things you probably want to do in this script include:
-#
-#   * Building folder structures in /var.  /var is the only non-tmpfs folder
-#     mounted read-write in the sandbox, and when a grain is first launched, it
-#     will start out empty.  It will persist between runs of the same grain, but
-#     be unique per app instance.  That is, two instances of the same app have
-#     separate instances of /var.
-#   * Preparing a database and running migrations.  As your package changes
-#     over time and you release updates, you will need to deal with migrating
-#     data from previous schema versions to new ones, since users should not have
-#     to think about such things.
-#   * Launching other daemons your app needs (e.g. mysqld, redis-server, etc.)
 
-# By default, this script does nothing.  You'll have to modify it as
-# appropriate for your application.
-cd /opt/app
-exit 0
+# Create a bunch of folders under the clean /var that php, nginx, and mysql expect to exist
+mkdir -p /var/lib/mysql
+mkdir -p /var/lib/nginx
+mkdir -p /var/lib/php5/sessions
+mkdir -p /var/log
+mkdir -p /var/log/mysql
+mkdir -p /var/log/nginx
+# Wipe /var/run, since pidfiles and socket files from previous launches should go away
+# TODO someday: I'd prefer a tmpfs for these.
+rm -rf /var/run
+mkdir -p /var/run
+mkdir -p /var/run/mysqld
+
+# Ensure mysql tables created
+HOME=/etc/mysql /usr/bin/mysql_install_db --force
+
+# Spawn mysqld, php
+HOME=/etc/mysql /usr/sbin/mysqld &
+/usr/sbin/php5-fpm --nodaemonize --fpm-config /etc/php5/fpm/php-fpm.conf &
+# Wait until mysql and php have bound their sockets, indicating readiness
+while [ ! -e /var/run/mysqld/mysqld.sock ] ; do
+    echo "waiting for mysql to be available at /var/run/mysqld/mysqld.sock"
+    sleep .2
+done
+while [ ! -e /var/run/php5-fpm.sock ] ; do
+    echo "waiting for php5-fpm to be available at /var/run/php5-fpm.sock"
+    sleep .2
+done
+
+# Start nginx.
+/usr/sbin/nginx -c /opt/app/.sandstorm/service-config/nginx.conf -g "daemon off;"
--- .sandstorm/setup.sh
+++ .sandstorm/setup.sh
@@ -4,27 +4,41 @@
 # - https://docs.sandstorm.io/en/latest/vagrant-spk/customizing/#setupsh
 
 set -euo pipefail
-# This is the ideal place to do things like:
-#
-#    export DEBIAN_FRONTEND=noninteractive
-#    apt-get update
-#    apt-get install -y nginx nodejs nodejs-legacy python2.7 mysql-server
-#
-# If the packages you're installing here need some configuration adjustments,
-# this is also a good place to do that:
-#
-#    sed --in-place='' \
-#            --expression 's/^user www-data/#user www-data/' \
-#            --expression 's#^pid /run/nginx.pid#pid /var/run/nginx.pid#' \
-#            --expression 's/^\s*error_log.*/error_log stderr;/' \
-#            --expression 's/^\s*access_log.*/access_log off;/' \
-#            /etc/nginx/nginx.conf
-
-# By default, this script does nothing.  You'll have to modify it as
-# appropriate for your application.
 
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
-apt-get install libreoffice
-
-exit 0
+apt-get install -y nginx php5-fpm php5-mysql php5-cli php5-curl git php5-dev mysql-server
+service nginx stop
+service php5-fpm stop
+service mysql stop
+systemctl disable nginx
+systemctl disable php5-fpm
+systemctl disable mysql
+# patch /etc/php5/fpm/pool.d/www.conf to not change uid/gid to www-data
+sed --in-place='' \
+        --expression='s/^listen.owner = www-data/;listen.owner = www-data/' \
+        --expression='s/^listen.group = www-data/;listen.group = www-data/' \
+        --expression='s/^user = www-data/;user = www-data/' \
+        --expression='s/^group = www-data/;group = www-data/' \
+        /etc/php5/fpm/pool.d/www.conf
+# patch /etc/php5/fpm/php-fpm.conf to not have a pidfile
+sed --in-place='' \
+        --expression='s/^pid =/;pid =/' \
+        /etc/php5/fpm/php-fpm.conf
+# patch /etc/php5/fpm/pool.d/www.conf to no clear environment variables
+# so we can pass in SANDSTORM=1 to apps
+sed --in-place='' \
+        --expression='s/^;clear_env = no/clear_env=no/' \
+        /etc/php5/fpm/pool.d/www.conf
+# patch mysql conf to not change uid
+sed --in-place='' \
+        --expression='s/^user\t\t= mysql/#user\t\t= mysql/' \
+        /etc/mysql/my.cnf
+# patch mysql conf to use smaller transaction logs to save disk space
+cat <<EOF > /etc/mysql/conf.d/sandstorm.cnf
+[mysqld]
+# Set the transaction log file to the minimum allowed size to save disk space.
+innodb_log_file_size = 1048576
+# Set the main data file to grow by 1MB at a time, rather than 8MB at a time.
+innodb_autoextend_increment = 1
+EOF
